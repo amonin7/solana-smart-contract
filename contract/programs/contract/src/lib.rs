0951a3e6f8e3e046b9a506a67852483ebd8d7edf
Andrey Minin <ewkjf@mail.ru>
2022-05-18 21:35:34 +0400

    stored the basic data on our contract

--- contract/programs/contract/src/lib.rs
+++ contract/programs/contract/src/lib.rs
@@ -6,17 +6,45 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod contract {
     use super::*;
     pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> Result <()> {
+        // Get a reference to the account.
+        // Note: We do &mut to get a "mutable reference" to base_account.
+        //     When we do this it actually gives us the power to make changes to base_account.
+        //     Otherwise, we'd simply be working w/ a "local copy" of base_account.
+        let base_account = &mut ctx.accounts.base_account;
+        // Initialize total_gifs.
+        base_account.total_gifs = 0;
         Ok(())
     }
 
-    pub fn another_simple_function(ctx: Context<AnotherSimpleFunction>) -> Result <()> {
-        println!("hello there!");
+    // Another function woo!
+    pub fn add_gif(ctx: Context<AddGif>) -> Result <()> {
+        // Get a reference to the account and increment total_gifs.
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.total_gifs += 1;
         Ok(())
     }
+
 }
 
 #[derive(Accounts)]
-pub struct StartStuffOff {}
+pub struct StartStuffOff<'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program <'info, System>,
+}
 
+// Specify what data you want in the AddGif Context.
+// Getting a handle on the flow of things :)?
 #[derive(Accounts)]
-pub struct AnotherSimpleFunction {}
+pub struct AddGif<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+}
+
+// Tell Solana what we want to store on this account.
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+}
